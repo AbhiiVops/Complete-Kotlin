ac2af0c5de8ed30f0399a93f357f6fe912d45fd2
Abhishek bhattacharjee <abhiias2003@gmail.com>
2022-05-13 16:22:42 +0530

    ConditionalsAndOperators

--- src/main/kotlin/Arrays/Lists/mutableLists.kt
+++ src/main/kotlin/Arrays/Lists/mutableLists.kt
@@ -3,6 +3,7 @@ package Arrays.Lists
 fun main() {
 
     val listOfStrings = mutableListOf("Box","Table","Chair","Honeyy","Abhii","Dhruv","Chair")
+    val subString = mutableListOf("Honeyyy","Abhii")
     listOfStrings[1] = "bed"
     listOfStrings.add("Babeyy")
     listOfStrings.add(4,"Babeyyyy")
@@ -11,8 +12,22 @@ fun main() {
 
     val arr = arrayOf("Fan","lights","mattress")
     listOfStrings.addAll(arr)
+    println(listOfStrings.get(3))
+    println(listOfStrings.containsAll(subString))
+    println(listOfStrings.remove("Box"))
     println(listOfStrings)
+    listOfStrings.set(1,"dhruv")
+    listOfStrings[1] = "dhruv"
+    // the above two methods are same
+    println(listOfStrings)
+    listOfStrings.clear()
+    println(listOfStrings)
+    listOfStrings.contains("dhruv")
+    listOfStrings.equals(subString)
+    listOfStrings.isEmpty()
+    listOfStrings.lastIndexOf("Honeyy")
 
+    println(listOfStrings)
 
 
 }
